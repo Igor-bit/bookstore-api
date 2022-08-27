d15ee90f3364754b06cb8ee21f1e6d5b8b3c1a76
IgorCordeiro <igorcordeirosze@gmail.com>
2022-08-27 13:37:23 -0300

    finalizando as classes de domínio

--- src/main/java/domain/Categoria.java
+++ src/main/java/domain/Categoria.java
@@ -1,15 +1,25 @@
 package domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
-public class Categoria {
+@Entity
+public class Categoria implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList()<>();
 	public Categoria() {
 		super();
